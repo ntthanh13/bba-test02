1eb29f1c8e090e5f028cb806eaefe89a34306d2b
test02 - TienThanh <tienthanh13579@gmail.com>
2026-09-18 19:57:58 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/calm-belt-2.js
@@ -0,0 +1,15 @@
+// Khai báo
+let devilFruit = "gomu-gomu";
+
+// Kiểm tra trạng thái thức tỉnh tương ứng và in ra màn hình
+if (devilFruit === "gomu-gomu") {
+    console.log("Gear 5 Unlocked!");
+} else if (devilFruit === "mera-mera") {
+    console.log("Flame Emperor!");
+} else if (devilFruit === "ope-ope") {
+    console.log("Room & Ope!");
+} else if (devilFruit === "none") {
+    console.log("Normal human");
+} else {
+    console.log("Unknow devil fruit");
+}
